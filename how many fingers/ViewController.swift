2d1b071c7d6ea1087a87c2e8d34f417296837cbf
Charlie <charliecuddy4@gmail.com>
2017-10-25 23:33:13 -0500

    random number generator

--- how many fingers/ViewController.swift
+++ how many fingers/ViewController.swift
@@ -10,6 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numGuess: UITextField!
+    
+    @IBOutlet weak var numAnswer: UILabel!
+    
+    @IBAction func guessButton(_ sender: Any) {
+        
+        print("You guessed")
+        if let numGuessText = numGuess.text {
+            if let numGuessInt = Int(numGuessText) {
+                
+                if numGuessInt > 5 {
+                    numAnswer.text = "How many fingers do you think are on one hand? Come on, guess a number that makes sense. "
+                } else {
+                
+                    let randomFingerNum = arc4random_uniform(6)
+        
+                    if numGuessInt == randomFingerNum {
+                        numAnswer.text = "You correctly guessed " + String(randomFingerNum) + " finger!"
+                    } else {
+                        numAnswer.text = "Sorry, the number of fingers was " + String(randomFingerNum) + ", not " + String(numGuessInt) + ". Guess again!"
+                    }
+                }
+            }
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
